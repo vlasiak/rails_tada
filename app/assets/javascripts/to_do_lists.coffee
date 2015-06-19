d10942a808c55bb7715eb0ebee3a2414d3e87290
Vasyl Lasiak <isnull29@gmail.com>
2015-06-19 18:28:22 +0300

    Implemented saving the data in the database and highlighting changes using ajax

--- app/assets/javascripts/to_do_lists.coffee
+++ app/assets/javascripts/to_do_lists.coffee
@@ -1,3 +1,12 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+#$("form input[type='text']").keyup(function(event) {
+#  if(event.keyCode == 27) {alert(1);}
+#})
+
+$('document').ready () ->
+  $("form input[type='text']").keyup (event) ->
+    if event.keyCode == 27
+      alert 1
--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -1,7 +1,26 @@
 class ToDoItemsController < ApplicationController
+
   def new
   end
 
   def create
+    @list = List.find params[:id]
+    item = @list.items.new(item_params)
+
+    respond_to do |format|
+      if item.save!
+        format.html { redirect_to to_do_lists_path, notice: 'List item was successfully created.' }
+        format.js { @current_item = item }
+      # else
+      #   format.html { render :new }
+      end
+    end
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:text)
   end
 end

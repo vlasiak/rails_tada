338d710a6b092a8ff659b620065fee2b833a7335
Vasyl Lasiak <isnull29@gmail.com>
2015-07-10 11:15:41 +0300

    Added grouping in tabs by the type of code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Lib', 'lib'
+  add_group 'Models', 'app/models'
+end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
